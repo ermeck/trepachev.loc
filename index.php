6ec48e6ffb9bd6560c2df3a4a4604fbedd2d5806
ermeck <ermeck@bk.ru>
2019-07-14 22:55:57 +0400

    update

--- index.php
+++ index.php
@@ -3,33 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        .wrapper {
+            display: grid;
+            grid-template-columns: 50% 50%;
+        }
+    </style>
 </head>
 <body>
-    <h3>01. База</h3>
-    <ul>
-        <li><a href="php-trepachev/01-Base/l-002/002.php">Lesson 002</a></li>
-        <li><a href="php-trepachev/01-Base/l-003/003.php">Lesson 003. Arrays</a></li>
-        <li><a href="php-trepachev/01-Base/l-004/004.php">Lesson 004. if-else, switch-case</a></li>
-        <li><a href="php-trepachev/01-Base/l-005/005.php">Lesson 005. Циклы foreach, while, for</a></li>
-        <li><a href="php-trepachev/01-Base/l-006/006.php">Lesson 006. Работа с математическими функциями</a></li>
-        <li><a href="php-trepachev/01-Base/l-007/007.php">Lesson 007. Работа со строковыми функциями</a></li>
-        <li><a href="php-trepachev/01-Base/l-008/008.php">Lesson 008. Задачи и функции для работы с массивами</a></li>
-        <li><a href="php-trepachev/01-Base/l-009/009.php">Lesson 009. Практика на комбинации стандартных функций</a></li>
-        <li><a href="php-trepachev/01-Base/l-010/010.php">Lesson 010. Работа с формами</a></li>
-        <li><a href="php-trepachev/01-Base/l-011/011.php">Lesson 011. Работа с датами</a></li>
-        <li><a href="php-trepachev/01-Base/l-012/012.php">Lesson 012. Работа с пользовательскими функциями</a></li>
-        <li><a href="php-trepachev/01-Base/l-013/013.php">Lesson 013. Работа с флагам</a></li>
-        <li><a href="php-trepachev/01-Base/l-014/014.php">Lesson 014. Работа с логическими значениями</a></li>
-        <li><a href="php-trepachev/01-Base/l-015/015.php">Lesson 015. Работа с вложенными циклами</a></li>
-    </ul>
-    <h3>07. Сессии и куки</h3>
-    <ul>
-        <li><a href="php-trepachev/07-SessionsAndCoocies/l-032/032.php">032. Работа с сессиями</a></li>
-        <li><a href="php-trepachev/"></a></li>
-    </ul>
-    <h3>Work</h3>
-    <ul>
-        <li><a href="php-trepachev/work/test_index.php">Test Main Page</a></li>
-    </ul>
+    <div class="wrapper">
+        <div class="php-block">
+            <h2>PHP</h2>
+            <h3>01. База</h3>
+            <ul>
+                <li><a href="php/01-Base/l-002/002.php">Lesson 002</a></li>
+                <li><a href="php/01-Base/l-003/003.php">Lesson 003. Arrays</a></li>
+                <li><a href="php/01-Base/l-004/004.php">Lesson 004. if-else, switch-case</a></li>
+                <li><a href="php/01-Base/l-005/005.php">Lesson 005. Циклы foreach, while, for</a></li>
+                <li><a href="php/01-Base/l-006/006.php">Lesson 006. Работа с математическими функциями</a></li>
+                <li><a href="php/01-Base/l-007/007.php">Lesson 007. Работа со строковыми функциями</a></li>
+                <li><a href="php/01-Base/l-008/008.php">Lesson 008. Задачи и функции для работы с массивами</a></li>
+                <li><a href="php/01-Base/l-009/009.php">Lesson 009. Практика на комбинации стандартных функций</a></li>
+                <li><a href="php/01-Base/l-010/010.php">Lesson 010. Работа с формами</a></li>
+                <li><a href="php/01-Base/l-011/011.php">Lesson 011. Работа с датами</a></li>
+                <li><a href="php/01-Base/l-012/012.php">Lesson 012. Работа с пользовательскими функциями</a></li>
+                <li><a href="php/01-Base/l-013/013.php">Lesson 013. Работа с флагам</a></li>
+                <li><a href="php/01-Base/l-014/014.php">Lesson 014. Работа с логическими значениями</a></li>
+                <li><a href="php/01-Base/l-015/015.php">Lesson 015. Работа с вложенными циклами</a></li>
+            </ul>
+            <h3>07. Сессии и куки</h3>
+            <ul>
+                <li><a href="php/07-SessionsAndCoocies/l-032/032.php">032. Работа с сессиями</a></li>
+                <li><a href="php/"></a></li>
+            </ul>
+            <h3>Work</h3>
+            <ul>
+                <li><a href="php/work/test_index.php">Test Main Page</a></li>
+            </ul>
+        </div>
+        <div class="js-block">
+            <h2>JavaScript</h2>
+            <h3>01. Base</h3>
+            <ul>
+                <li><a href="js/01-Base/l-002/002.html">Lesson 002. Основы языка</a></li>
+            </ul>
+        </div>
+    </div>
+    
 </body>
 </html>
